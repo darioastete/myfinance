6bf051a995d341d309a305f2a8eeb0b27d1c9719
Carlos Astete <carlosastete@culqi.com>
2024-08-03 23:27:18 +1000

    chore: adding environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  build: {
-    inlineStylesheets: "always"
-  },
-  compressHTML: true,
-  prefetch: true,
-  devToolbar: {
-    enabled: false
-  },
-  output: "server",
-  integrations: [tailwind(), db()],
-  adapter: vercel()
+	build: {
+		inlineStylesheets: "always",
+	},
+	compressHTML: true,
+	prefetch: true,
+	devToolbar: {
+		enabled: false,
+	},
+	output: "server",
+	integrations: [tailwind(), db()],
+	adapter: vercel(),
 });
