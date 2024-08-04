153405abb4f310d9aed69d97aa8ba02a21f7166c
Carlos Astete <carlosastete@culqi.com>
2024-08-05 00:18:07 +1000

    fix: date udpate v2 tzDate

--- src/common/utils/createdAt.util.ts
+++ src/common/utils/createdAt.util.ts
@@ -1,11 +1,13 @@
-import { format } from "@formkit/tempo";
+import { format, tzDate } from "@formkit/tempo";
 
 export default function (): number {
-  const date = format({
-    date: new Date(),
-    format: "YYYY-MM-DD HH:mm",
-    tz: "Australia/Sydney",
-  });
-
-  return new Date(date).getTime();
+  console.log("AYDNEY: ", tzDate(new Date(), "Australia/Sydney").getTime());
+  // const date = format({
+  //   date: new Date(),
+  //   format: "YYYY-MM-DD HH:mm:ss",
+  //   tz: "Australia/Sydney",
+  // });
+  // return new Date(date).getTime();
+  const date = tzDate(new Date(), "Australia/Sydney").getTime();
+  return date;
 }
