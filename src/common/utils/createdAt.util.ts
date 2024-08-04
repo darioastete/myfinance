c5f14670956566565563f514c3c77c07ecc442ef
Carlos Astete <carlosastete@culqi.com>
2024-08-05 00:25:55 +1000

    fix: date udpate v3 moment

--- src/common/utils/createdAt.util.ts
+++ src/common/utils/createdAt.util.ts
@@ -1,13 +1,9 @@
-import { format, tzDate } from "@formkit/tempo";
+import { tzDate } from "@formkit/tempo";
+import moment from "moment-timezone";
 
 export default function (): number {
-  console.log("AYDNEY: ", tzDate(new Date(), "Australia/Sydney").getTime());
-  // const date = format({
-  //   date: new Date(),
-  //   format: "YYYY-MM-DD HH:mm:ss",
-  //   tz: "Australia/Sydney",
-  // });
-  // return new Date(date).getTime();
-  const date = tzDate(new Date(), "Australia/Sydney").getTime();
-  return date;
+  // const date = tzDate(new Date(), "Australia/Sydney").getTime();
+  const dateInSydney = moment.tz(new Date(), "Australia/Sydney").valueOf();
+
+  return dateInSydney;
 }
