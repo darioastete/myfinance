3bd0591ca7b0e6e57651e46056759330a10d7a7a
Carlos Astete <carlosastete@culqi.com>
2024-08-19 01:18:40 +1000

    --amend

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,24 @@
-// import { db } from 'astro:db';
+import { db, Incomes, Expenses } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
   // TODO
+  await db.insert(Incomes).values([
+    {
+      id: "1",
+      user_id: "1",
+      amount: 100,
+      description: "First income",
+      createdAt: 1723700621,
+    },
+  ]);
+  await db.insert(Expenses).values([
+    {
+      id: "1",
+      user_id: "1",
+      amount: 50,
+      description: "First expense",
+      createdAt: 1723700621,
+    },
+  ]);
 }
